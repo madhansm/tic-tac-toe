19357a6509db9a60a8cb1cf9e5ebb4b74d12aa91
madhansm <madhan.samiappan@hotmail.com>
2022-01-26 13:30:28 +0530

    bare-bone working game complete

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
 let board = [];
 
-// board empty
-for (i = 0 ; i <9 ; i++){
-    board.push(null);
+
+
+//display board
+function displayBoard(){
+let grid = document.querySelector(".grid");
+
+for (i = 0; i < 9; i++) {
+    board[i] = null;
+    let boxElement = document.createElement('div');
+    boxElement.classList.add('cell');
+    boxElement.setAttribute('id', `box${i}`);
+    grid.appendChild(boxElement);
+    
+}
 }
 
-// board display
-console.log(board[0], board[1], board[2]);
-console.log(board[3], board[4], board[5]);
-console.log(board[6], board[7], board[8]);
-
-// entry check
-if(board[5]){
-    console.log('cannot')
-} else {
-    board[5] = 'o';
+// board empty  
+function clearBoard() {
+let cellList = document.querySelectorAll('.cell');
+cellList.forEach(cell => {
+    cell.remove();
+});
+displayBoard();
 }
-
-
-// win check
-if (
-       (board[0] === board[1] && board[0] === board[2] && (board[0] && board[1] && board[2]))
-    || (board[0] === board[3] && board[0] === board[6] && (board[0] && board[3] && board[6])) 
-    || (board[0] === board[4] && board[0] === board[8] && (board[0] && board[4] && board[8]))
-    || (board[1] === board[4] && board[1] === board[7] && (board[1] && board[4] && board[7]))
-    || (board[2] === board[5] && board[2] === board[8] && (board[2] && board[5] && board[8]))
-    || (board[2] === board[4] && board[2] === board[6] && (board[2] && board[4] && board[6]))
-    || (board[3] === board[4] && board[3] === board[5] && (board[3] && board[4] && board[5])) 
-    || (board[6] === board[7] && board[6] === board[8] && (board[6] && board[7] && board[8]))
-){
-    console.log('win');
-}
-console.log(board);
 
 //person factory
 const playerFactory = (playerName,sign) => {
@@ -45,8 +37,66 @@ console.log(playerX);
 let playerO = playerFactory('Scott','o');
 console.log(playerO);
 
+
 //current player
 let currentPlayer = playerX;
 
+displayBoard();
+
+// board cells select
+function gamePlay() {
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => {
+    cell.addEventListener("click", () => markEntry(cell));
+});
+}
+gamePlay();
+function markEntry(c) {
+    let value = c.getAttribute('id')[3];
+    
+    console.log(board[value]);
+    // entry check
+    if (!board[value]) {
+        board[value] = currentPlayer.sign;
+        c.append(currentPlayer.sign);
+        console.log(c);
+
+
+        winCheck();
+        if (currentPlayer === playerX){
+            currentPlayer = playerO;
+        } else {
+                currentPlayer = playerX;
+        }
+        
+    } else {
+        console.log('cannot')
+    }
+}
+
+
+
+
+// win check
+function winCheck() {
+
+    if (
+        (board[0] === board[1] && board[0] === board[2] && (board[0] && board[1] && board[2]))
+        || (board[0] === board[3] && board[0] === board[6] && (board[0] && board[3] && board[6]))
+        || (board[0] === board[4] && board[0] === board[8] && (board[0] && board[4] && board[8]))
+        || (board[1] === board[4] && board[1] === board[7] && (board[1] && board[4] && board[7]))
+        || (board[2] === board[5] && board[2] === board[8] && (board[2] && board[5] && board[8]))
+        || (board[2] === board[4] && board[2] === board[6] && (board[2] && board[4] && board[6]))
+        || (board[3] === board[4] && board[3] === board[5] && (board[3] && board[4] && board[5]))
+        || (board[6] === board[7] && board[6] === board[8] && (board[6] && board[7] && board[8]))
+    ) {
+        console.log('win');
+        clearBoard();
+        gamePlay();
+    }
+};
+console.log(board);
+
 
 //gameplay
+

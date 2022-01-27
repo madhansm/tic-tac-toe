b2094c0119eca88f9350fbc7fb503c71b78c2c8d
madhansm <madhan.samiappan@hotmail.com>
2022-01-27 09:26:15 +0530

    restructured code inside modules

--- script.js
+++ script.js
@@ -1,112 +1,104 @@
-let board = [];
+const gameInit = (function(){
+    const playerFactory = (playerName, sign) => {
+        return { playerName, sign }
+    };
 
-function displayBoard() {
-    let grid = document.querySelector(".grid");
-
-    for (i = 0; i < 9; i++) {
-        board[i] = null;
-        let boxElement = document.createElement('div');
-        boxElement.classList.add('cell');
-        boxElement.setAttribute('id', `box${i}`);
-        grid.appendChild(boxElement);
-
+    let theGame = {
+        board: [],
+        playerX: playerFactory('Matt', 'x'),
+        playerO: playerFactory('Scott', 'o'),
     }
-}
 
-function clearBoard() {
-    let cellList = document.querySelectorAll('.cell');
-    cellList.forEach(cell => {
-        cell.remove();
-    });
-    displayBoard();
-}
+    let currentPlayer = theGame.playerX;
 
-const playerFactory = (playerName,sign) => {
-    return {playerName, sign}
-};
+    function displayBoard() {
+        let grid = document.querySelector(".grid");
 
-//player input
-let playerX = playerFactory('Matt','x');
-console.log(playerX);
+        for (i = 0; i < 9; i++) {
+            theGame.board[i] = null;
+            let boxElement = document.createElement('div');
+            boxElement.classList.add('cell');
+            boxElement.setAttribute('id', `box${i}`);
+            grid.appendChild(boxElement);
 
-let playerO = playerFactory('Scott','o');
-console.log(playerO);
+        }
+    }
 
+    function clearBoard() {
+        let cellList = document.querySelectorAll('.cell');
+        cellList.forEach(cell => {
+           cell.remove();
+        });
+        displayBoard();
+    }
+    return {theGame, currentPlayer, clearBoard}
+})();
 
-//current player
-let currentPlayer = playerX;
+const gaming = (function(){
+    function gamePlay() {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(cell => {
+            cell.addEventListener("click", () => markEntry(cell));
+        });
+    }
 
-displayBoard();
+    function markEntry(c) {
+        let value = c.getAttribute('id')[3];
 
-// board cells select
-function gamePlay() {
-    const cells = document.querySelectorAll(".cell");
-    cells.forEach(cell => {
-        cell.addEventListener("click", () => markEntry(cell));
-    });
-}
-gamePlay();
-function markEntry(c) {
-    let value = c.getAttribute('id')[3];
-    
-    console.log(board[value]);
+        console.log(gameInit.theGame.board[value]);
     // entry check
-    if (!board[value]) {
-        board[value] = currentPlayer.sign;
-        c.append(currentPlayer.sign);
-        console.log(c);
-
-
-        winCheck();
-        tieCheck();
-        if (currentPlayer === playerX){
-            currentPlayer = playerO;
+        if (!gameInit.theGame.board[value]) {
+            gameInit.theGame.board[value] = gameInit.currentPlayer.sign;
+            c.append(gameInit.currentPlayer.sign);
+            console.log(c);
+            winCheck();
+            tieCheck();
+            if (gameInit.currentPlayer === gameInit.theGame.playerX) {
+                gameInit.currentPlayer = gameInit.theGame.playerO;
+            } else {
+                gameInit.currentPlayer = gameInit.theGame.playerX;
+            }
         } else {
-                currentPlayer = playerX;
-        }
-        
-    } else {
         console.log('cannot')
-    }
-}
-
-
-function tieCheck() {
-    let tie = true;
-    board.forEach(cell => {
-        if(cell){
-            tie *= true;
-        } else {
-            tie *= false;
         }
-    });
-    if(tie){
-        console.log('tie');
-        clearBoard();
-        gamePlay();
     }
-}
+
+    function tieCheck() {
+        let tie = true;
+        gameInit.theGame.board.forEach(cell => {
+            if (cell) {
+                tie *= true;
+            } else {
+                tie *= false;
+            }
+        });
+        if (tie) {
+            console.log('tie');
+            clearBoard();
+            gamePlay();
+        }
+    }
 
 // win check
-function winCheck() {
+    function winCheck() {
+        let board = gameInit.theGame.board;
+        if (
+            (board[0] === board[1] && board[0] === board[2] && (board[0] && board[1] && board[2]))
+            || (board[0] === board[3] && board[0] === board[6] && (board[0] && board[3] && board[6]))
+            || (board[0] === board[4] && board[0] === board[8] && (board[0] && board[4] && board[8]))
+            || (board[1] === board[4] && board[1] === board[7] && (board[1] && board[4] && board[7]))
+            || (board[2] === board[5] && board[2] === board[8] && (board[2] && board[5] && board[8]))
+            || (board[2] === board[4] && board[2] === board[6] && (board[2] && board[4] && board[6]))
+            || (board[3] === board[4] && board[3] === board[5] && (board[3] && board[4] && board[5]))
+            || (board[6] === board[7] && board[6] === board[8] && (board[6] && board[7] && board[8]))
+        ){
+            console.log('win');
+            gameInit.clearBoard();
+            gamePlay();
+        }
+    };
+    return {gamePlay}
+})();
 
-    if (
-        (board[0] === board[1] && board[0] === board[2] && (board[0] && board[1] && board[2]))
-        || (board[0] === board[3] && board[0] === board[6] && (board[0] && board[3] && board[6]))
-        || (board[0] === board[4] && board[0] === board[8] && (board[0] && board[4] && board[8]))
-        || (board[1] === board[4] && board[1] === board[7] && (board[1] && board[4] && board[7]))
-        || (board[2] === board[5] && board[2] === board[8] && (board[2] && board[5] && board[8]))
-        || (board[2] === board[4] && board[2] === board[6] && (board[2] && board[4] && board[6]))
-        || (board[3] === board[4] && board[3] === board[5] && (board[3] && board[4] && board[5]))
-        || (board[6] === board[7] && board[6] === board[8] && (board[6] && board[7] && board[8]))
-    ) {
-        console.log('win');
-        clearBoard();
-        gamePlay();
-    }
-};
-console.log(board);
-
-
-//gameplay
-
+gameInit.clearBoard();
+gaming.gamePlay();
